717021222fb90fb4b134722f7261920ea6196b06
Ethan Obott <ethanobott6002@gmail.com>
2023-09-04 22:18:29 +0100

    added a function to handle the help command

--- mainApp.py
+++ mainApp.py
@@ -1,10 +1,60 @@
+import sys
+#! Function to Initially display instructions
 
-# * Function to read instructions from text file
+
 def displayInstructions():
+
+    # *opening and reading from the "instructions.txt" file
     with open("instructions.txt", "r", encoding="utf-8") as file:
         return file.read()
 
 
-print(displayInstructions())
+#! Function to handle >HELP command
 
-print()
+def helpCmdFunction():
+
+    # * code to print out instructions when the user types in "HELP"
+    instructions_func = displayInstructions()
+    print("\n")
+    return instructions_func
+
+#! Function to handle >ADD command
+
+
+def addCmdFunction():
+    return "addCmdFunction works!"
+
+
+# ! Function to handle commands
+
+def handleCommands():
+
+    allowed_commands = ("ADD", "REMOVE", "VIEW", "HELP", "QUIT")
+
+    # ** While true to keep accepting input from the user
+    while True:
+        user_cmd_input = input("> ").upper()
+
+        # * if statement to check if the command the user typed is in the list
+        if user_cmd_input not in allowed_commands:
+            print("Command not recognised (Type \"HELP\" to see list of commands)")
+            print("\n")
+
+        # * if statement to activate the function that handles the "HELP" command (helpCmdFunction)
+
+        if user_cmd_input == "HELP":
+            print(helpCmdFunction())
+            print("\n")
+
+        # * if statement to activate the function that handles the "ADD" command (helpCmdFunction)
+
+        if user_cmd_input == "ADD":
+            print(addCmdFunction())
+            print("\n")
+
+        # * if statement to activate the function that handle the "QUIT" by calling sys.exit
+        if user_cmd_input == "QUIT":
+            sys.exit("Okay, until next time")
+
+
+print(handleCommands())
