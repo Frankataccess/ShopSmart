e656be13f054c7f1198123d42e0fcdaddf20f179
Ethan Obott <ethanobott6002@gmail.com>
2023-09-05 06:17:48 +0100

    created a function to remove items from the list

--- mainApp.py
+++ mainApp.py
@@ -21,15 +21,16 @@ def helpCmdFunction():
 #! Function to handle >ADD command
 
 
-def addCmdFunction():
+shopping_list = {}
+
+
+def addCmdFunction(shopping_list):
 
     while True:
 
-        shopping_list = {}
-
         # * accepts input for shopping item
 
-        item_input = input("Enter an Item: ")
+        item_input = input("Enter an Item: ").title()
 
         # * if statement to check if the number of characters entered is more than 30
         if len(item_input) > 30:
@@ -63,12 +64,32 @@ def addCmdFunction():
 
     return shopping_list
 
+#! Function to handle >REMOVE command
+
+
+def removeCmdFunction(shopping_list):
+    while True:
+        # * Accept input for the item to remove
+        item_to_remove = input("Enter an item to remove: ").strip().title()
+
+        # * Check if the item is in the shopping list
+        if item_to_remove in shopping_list:
+            del shopping_list[item_to_remove]
+            print("Item removed (Type \"VIEW\"to view shopping)")
+            return ""
+
+        # * If statement to return an error if the item is in not the shopping list
+        if item_to_remove not in shopping_list:
+            print("Please enter an item that is in your shopping list")
+            print()
+            continue
 
 # ! Function to handle commands
 
+
 def handleCommands():
 
-    allowed_commands = ("ADD", "REMOVE", "VIEW", "HELP", "QUIT")
+    allowed_commands = ("ADD", "REMOVE", "VIEW", "HELP", "QUIT", "TOTAL")
 
     # ** While true to keep accepting input from the user
     while True:
@@ -79,17 +100,32 @@ def handleCommands():
             print("Command not recognised (Type \"HELP\" to see list of commands)")
             print("\n")
 
-        # * if statement to activate the function that handles the "HELP" command (helpCmdFunction)
+        # !if statement to activate the function that handles the "HELP" command (helpCmdFunction)
 
         if user_cmd_input == "HELP":
             print(helpCmdFunction())
             print("\n")
 
-        # * if statement to activate the function that handles the "ADD" command (helpCmdFunction)
+        # !if statement to activate the function that handles the "ADD" command (addCmdFunction)
 
         if user_cmd_input == "ADD":
-            print(addCmdFunction())
+            print(addCmdFunction(shopping_list))
             print("\n")
+
+        # !if statement to activate the function that handles the "ADD" command (addCmdFunction)
+
+        if user_cmd_input == "REMOVE":
+            # * if statement to print an error message if the user tries to remove an item from an empty shopping list
+
+            if not shopping_list:
+                print(
+                    "There's nothing in the shopping list to remove (Type \"ADD\" to add an item to the list)")
+                print()
+                continue
+            print(removeCmdFunction(shopping_list))
+            print("\n")
+
+        # !if statement to activate the function that handles the "QUIT" command
 
         # * if statement to activate the function that handle the "QUIT" by calling sys.exit
         if user_cmd_input == "QUIT":
@@ -97,3 +133,8 @@ def handleCommands():
 
 
 print(handleCommands())
+
+"""
+what if the user adds an item that's not in the shopping list?
+
+"""
